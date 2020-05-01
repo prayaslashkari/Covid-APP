94b0343523fbd3960d060c3dd3f9c556e2e9fd7b
Prayas Lashkari <rock198269@gmail.com>
2020-05-02 02:15:54 +0530

    FINAL
    FINAL FINAL

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import {Cards, Chart, CountryPicker } from './components';
+
+import styles from './App.module.css';
+
+import {fetchData} from './api';
+
+import Avatar from '@material-ui/core/Avatar';
+
+class App extends Component {
+    state = {
+        data : {},
+        country : '',
+    }
+
+    async componentDidMount()
+    {
+        const fetchedData = await fetchData();
+        this.setState({data : fetchedData})
+    }
+
+    handleCountryChange = async (country) =>
+    {
+        const fetchedData = await fetchData(country);
+        console.log(fetchedData);
+
+        this.setState({data : fetchedData, country : country});
+    }
+
+    render() {
+
+        const {data, country} = this.state;
+
+        return (
+            <div className = {styles.container}>
+
+                <Cards data = {data}/>
+                <CountryPicker handleCountryChange = {this.handleCountryChange} />
+                <Chart data = {data} country = {country}/>
+
+                <Avatar alt="Remy Sharp" src="./img/prayas.jpg" />
+            
+            </div>
+         );
+    }
 }
 
 export default App;
